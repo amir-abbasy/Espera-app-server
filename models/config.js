51de04e8482b82550af24cab408480b7e926003a
Amir-Abbasy-k <amirabbasyk@gmail.com>
2022-05-25 00:27:52 +0530

    deply versal

--- models/config.js
+++ models/config.js
@@ -10,87 +10,14 @@ const database = ENV.NODE_ENV ==  'dev' ? ENV.DB2 : ENV.DB;
 
 console.log(host, user, password, database);
 
-const pool  = mysql.createPool({
-    connectionLimit: 10,
-    host,
-    user,
-    password,
-    database,
-
-    // host: "127.0.0.1",
-    // user: "root",
-    // password: "amir",
-    // database: "coupen"
-})
-
-
-// var pool_ = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "amir",
-//     database: "coupen"
-//   });
-
-
-
-  //  var con = pool_.connect(function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  //   return pool_;
-  // });
-
-
-  // con.connect(function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  //   var sql = "INSERT INTO users (user_id, fullname, username, user_password,mobile) VALUES ('user2','fullname',  'Amir 2', '123456', 'mobile')";
-  //   con.query(sql, function (err, result) {
-  //     if (err) throw err;
-  //     console.log("1 record inserted");
-  //   });
-  // });
-
-// function run(){
-//     var sql = "INSERT INTO users (user_id, fullname, username, user_password,mobile) VALUES ('user2','fullname',  'Amir 2', '123456', 'mobile')";
-//     pool_.query(sql, function (err, result) {
-//       if (err) throw err;
-//       console.log("1 record inserted");
-//     });
-// }
-
-// run()
-
-
-// module.exports = pool;
-
-
-
-// var mysql = require('mysql');
-
- // connect to the db
  dbConnectionInfo = {
-   connectionLimit: 5, //mysql connection pool length
-   host: "localhost",
-//    port: "3306",
-   user: "root",
-   password: "amir",
-   database: "coupen"
- };
+   connectionLimit: 10,
+   host,
+   user,
+   password,
+   database,
+  };
  
- //For mysql single connection
- /* var dbconnection = mysql.createConnection(
-         dbConnectionInfo
- ); 
- dbconnection.connect(function (err) {
-    if (!err) {
-        console.log("Database is connected ... nn");
-    } else {
-        console.log("Error connecting database ... nn");
-    }
-}); 
-
-*/
-
 //create mysql connection pool
 var dbconnection = mysql.createPool(
   dbConnectionInfo
